ef5e606f40c9cf3af0efcf0738f08902981f9009
piotrgolab <piotr.golab@gmail.com>
2018-06-06 16:45:38 +0200

    Add files via upload

---
+++ WebSocket/src/main/java/websocket/ChatEndpoint.java
@@ -0,0 +1,49 @@
+package websocket;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import model.Message;
+
+@ServerEndpoint(value = "/chat/{username}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
+public class ChatEndpoint {
+    private Session session;
+    private static final Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
+    private static HashMap<String, String> users = new HashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
+
+
+    }
+
+    @OnMessage
+    public void onMessage(Session session, Message message) throws IOException, EncodeException {
+
+    	
+    }
+
+    @OnClose
+    public void onClose(Session session) throws IOException, EncodeException {
+
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        
+    }
+
+
+}
